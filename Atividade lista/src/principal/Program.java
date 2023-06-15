2166cf6f7b9d0d06c6a223035e6a30244e6b3411
Kamylla Morais Viana <kamyllamoraisviana@hotmail.com>
2023-06-14 21:23:32 -0300

    Atualização atividade lis

--- Atividade lista/src/principal/Program.java
+++ Atividade lista/src/principal/Program.java
@@ -11,45 +11,83 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		/* SEM CONSTRUTOR 
 		Scanner sc = new Scanner(System.in);
 		Pessoa pessoa;
-		
+
 		List<Pessoa> adicionar = new ArrayList<>();
 		char opcao;
-		
-		
+
 		do {
-			pessoa = new Pessoa () ;
-			
+			pessoa = new Pessoa();
+
 			System.out.print("Qual o nome: ");
 			pessoa.setNome(sc.nextLine());
-			
+
 			System.out.print("Qual a idade: ");
 			pessoa.setIdade(sc.nextInt());
 
 			System.out.print("Qual o telefone: ");
 			pessoa.setTelefone(sc.nextInt());
-			
+
 			adicionar.add(pessoa);
 			System.out.print("Deseja realizar outro cadastro? (s/n)");
 			opcao = sc.next().charAt(0);
-			
-			//O comando abaixo é utilizado para limpar a memória do Scanner... 
-			sc.nextLine();
-			
-			
-		}
-		
-		while(Character.toLowerCase(opcao) == 's');
 
-		
+			// O comando abaixo é utilizado para limpar a memória do Scanner e evitar bug...
+			sc.nextLine();
+
+		}
+
+		while (Character.toLowerCase(opcao) == 's');
+
 		for (Pessoa p : adicionar) {
 			System.out.println("\nCadastro:");
 			System.out.println(p);
 		}
 
-		sc.close();
+		sc.close(); */
+		
+		
+		Scanner sc = new Scanner(System.in);
+		Pessoa pessoa;
 
+		List<Pessoa> adicionar = new ArrayList<>();
+		char opcao;
+
+		do {
+			
+
+			System.out.print("Qual o nome: ");
+			String nome = sc.nextLine();
+			
+			System.out.print("Qual a idade: ");
+			int idade = sc.nextInt();
+			
+			System.out.print("Qual o telefone: ");
+			int telefone = sc.nextInt();
+			
+			pessoa = new Pessoa(nome, idade, telefone);
+			
+			adicionar.add(pessoa);
+			System.out.print("Deseja realizar outro cadastro? (s/n)");
+			opcao = sc.next().charAt(0);
+
+			// O comando abaixo é utilizado para limpar a memória do Scanner e evitar bug...
+			sc.nextLine();
+
+		}
+
+		while (Character.toLowerCase(opcao) == 's');
+
+		for (Pessoa p : adicionar) {
+			System.out.println("\nCadastro:");
+			System.out.println(p);
+		}
+
+		sc.close(); 
+		
+	
 	}
 
 }
